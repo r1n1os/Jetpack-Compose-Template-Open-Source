5056960d91dc69286094ed14290c2e595913a78a
r1n1os <evagoras.frang@gmail.com>
2025-07-26 14:07:56 +0300

    create folder structure

---
+++ app/src/main/java/com/r1n1os/jetpackcomposetemplateopensource/JetpackComposeTemplateOpenSource.kt
@@ -0,0 +1,8 @@
+package com.r1n1os.jetpackcomposetemplateopensource
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class JetpackComposeTemplateOpenSource: Application() {
+}
--- app/src/main/java/com/r1n1os/jetpackcomposetemplateopensource/presentation/MainActivity.kt
+++ app/src/main/java/com/r1n1os/jetpackcomposetemplateopensource/presentation/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.r1n1os.jetpackcomposetemplateopensource
+package com.r1n1os.jetpackcomposetemplateopensource.presentation
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
@@ -12,7 +12,9 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.r1n1os.jetpackcomposetemplateopensource.ui.theme.JetpackComposeTemplateOpenSourceTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
